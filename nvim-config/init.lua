178e775a5000459451dc094e70dddf356b8e1e8b
Novo <abhi.mekin@gmail.com>
2025-04-13 09:16:58 +0530

    Initial-config

---
+++ nvim-config/init.lua
@@ -0,0 +1,6 @@
+require("nothenovo.config.remap")
+require("nothenovo.config.lazy")
+require("nothenovo.config.plugconfig")
+vim.opt.clipboard = "unnamedplus"
+vim.wo.relativenumber = true
+vim.wo.number = true
---
+++ nvim-config/lua/nothenovo/plugins/mini-pairs.lua
@@ -0,0 +1,22 @@
+return{ 'echasnovski/mini.pairs', 
+	version = false, 
+	opts = {  -- In which modes mappings from this `config` should be created
+	  modes = { insert = true, command = false, terminal = false },
+	  mappings = {
+	    ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+	    ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+	    ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+
+	    [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+	    [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+	    ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+
+	    ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+	    ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+	    ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+},
+config = function(_, opts)
+    LazyVim.mini.pairs(opts)
+  end,
+},
+}
