d728b90f98001fab727c166b70b9234015ea2439
NOVO-17 <abhi.mekin@gmail.com>
2025-05-01 20:48:47 +0530

    Major_1.1 added LSP added treesitter added treesitterPlayground added harpoon added undoTree

--- nvim-config/lua/nothenovo/plugins/treesitter.lua
+++ nvim-config/lua/nothenovo/plugins/treesitter.lua
@@ -3,26 +3,15 @@ return{
     {'do': ':TSUpdate'},
     opts = {    
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c","python","cpp","java","javascript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { "c","help","python","cpp","java","javascript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
     
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
         auto_install = true,
     
-        ignore_install = {},
     
         highlight = {
         enable = true,
-        disable = {},
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-    
         additional_vim_regex_highlighting = false,
     }
   }
